2fc484efc4e44124931ca398aa994839591da950
sjrpractice <40860767+sjrpractice@users.noreply.github.com>
2019-02-15 10:38:12 -0500

    Update server.R

--- server.R
+++ server.R
@@ -1,29 +1,20 @@
 library(shiny)
 library(datasets)
 
-# We tweak the "am" field to have nicer factor labels. Since this doesn't
-# rely on any user inputs we can do this once at startup and then use the
-# value throughout the lifetime of the application
 judges <- USJudgeRatings
 judges$RTEN<-round(judges$RTEN, digits = 0)
-#mpgData$am <- factor(mpgData$am, labels = c("Automatic", "Manual"))
 
-# Define server logic required to plot various variables against mpg
 shinyServer(function(input, output) {
   
-  # Compute the forumla text in a reactive expression since it is 
-  # shared by the output$caption and output$mpgPlot expressions
+
   formulaText <- reactive({
     paste(input$variable, "~ RTEN")
   })
   
-  # Return the formula text for printing as a caption
   output$caption <- renderText({
     formulaText()
   })
   
-  # Generate a plot of the requested variable against mpg and only 
-  # include outliers if requested
   output$judgesPlot <- renderPlot({
     boxplot(as.formula(formulaText()), 
             data = judges,
