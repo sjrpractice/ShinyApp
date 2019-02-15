b07d78bd703a77549a5efa12b5d83da0e7bb361c
sjrpractice <40860767+sjrpractice@users.noreply.github.com>
2019-02-15 10:38:34 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,13 +1,9 @@
 library(shiny)
 
-# Define UI for miles per gallon application
 shinyUI(pageWithSidebar(
   
-  # Application title
   headerPanel("Lawyer contacts and interactions with on judge retention ratings"),
   
-  # Sidebar with controls to select the variable to plot against mpg
-  # and to specify whether outliers should be included
   sidebarPanel(
     selectInput("variable", "Variable:",
                 list("Number of contacts of lawyer with judge" = "CONT", 
@@ -18,7 +14,6 @@ shinyUI(pageWithSidebar(
     checkboxInput("outliers", "Show outliers", FALSE)
   ),
   
-  # Show the caption and plot of the requested variable against mpg
   mainPanel(
     h3(textOutput("This is where the text goes."),
     verbatimTextOutput("Here is more text!")),
